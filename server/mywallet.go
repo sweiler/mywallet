925fdccccd975fbc65d6d5fc19ffa15257c87f3c
Simon Weiler <mail@simon-weiler.de>
2014-11-22 20:22:35 +0100

    Simple web-server functionality added

--- server/mywallet.go
+++ server/mywallet.go
@@ -1,5 +1,13 @@
 package main
 
+import "net/http"
+import "fmt"
+
+func defaultHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "Hi @ all, i can use Ümlauts")
+}
+
 func main() {
-	
+	http.HandleFunc("/", defaultHandler)
+	http.ListenAndServe(":5678", nil)
 }
