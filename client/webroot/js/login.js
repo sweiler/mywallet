5124cd1fa1b5d77b15f2c198ea30e46aa841f06b
Simon Weiler <mail@simon-weiler.de>
2014-12-20 16:52:25 +0100

    Sign up implemented. Closes #6

--- client/webroot/js/login.js
+++ client/webroot/js/login.js
@@ -1,7 +1,7 @@
 define(function (require, exports) {
 
 	var app = require("main");
-	var login_url = "http://www.mywallet.de/server/users/";
+	var login_url = "http://www.mywallet.de/server/users";
 	
 	var form = $("<form class='form-signin' role='form'></form>");
 	$("<h2 class='form-signin-heading'>Anmelden</h2>").appendTo(form);
@@ -13,6 +13,8 @@ define(function (require, exports) {
 			"class='form-control' placeholder='Passwort' required />").appendTo(form);
 	$("<button class='btn btn-lg btn-primary btn-block' " +
 			"type='submit'>Anmelden</button>").appendTo(form);
+	var signUp = $("<button class='btn btn-lg btn-default btn-block'" +
+			" type='button'>Registrieren</button>").appendTo(form);
 	
 	var warning = $("<div class='alert alert-warning'>Login fehlerhaft!</div>");
 	form.prepend(warning);
@@ -25,6 +27,34 @@ define(function (require, exports) {
 		return form;
 	};
 	
+	signUp.click(function () {
+		var username = userInput.val();
+		var password = pwdInput.val();
+		warning.hide();
+		if(username == "" || password == "") {
+			warning.text("Bitte fülle beide Felder aus.");
+			warning.show();
+			return;
+		}
+		var usrObj = {username: username, password: password};
+		$.ajax({
+			type : "POST",
+			url : login_url,
+			data : JSON.stringify(usrObj),
+			success : function () {
+				form.submit();
+			},
+			error : function (xhr, status, text) {
+				if(xhr.status == 409) {
+					warning.text("Dieser Benutzername ist bereits vergeben.");
+				} else {
+					warning.text("Ein Fehler ist aufgetreten: " + text);
+				}
+				warning.show();
+			}
+		});
+	});
+	
 	form.submit(function () {
 		var username = userInput.val();
 		var password = pwdInput.val();
@@ -32,12 +62,17 @@ define(function (require, exports) {
 		warning.hide();
 		$.ajax({
 			type : "GET",
-			url : login_url + username,
+			url : login_url + "/" + username,
 			headers : {
 				"X-Mywallet-Auth" : JSON.stringify(usrObj)
 			},
 			dataType : "json",
 			error : function (xhr, status, text) {
+				if(xhr.status == 403) {
+					warning.text("Anmeldedaten sind falsch!");
+				} else {
+					warning.text("Anmeldung nicht möglich: " + text);
+				}
 				warning.show();
 				pwdInput.val("");
 			},
